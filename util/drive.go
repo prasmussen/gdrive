28b7ba078faa0049e45e73d1c6f37d6dceba762e
Petter Rasmussen <petter.rasmussen@gmail.com>
2014-02-15 21:29:07 +0100

    Helper function to create a list of parent id's

--- util/drive.go
+++ util/drive.go
@@ -2,6 +2,8 @@ package util
 
 import (
     "fmt"
+    "strings"
+    "code.google.com/p/google-api-go-client/drive/v2"
 )
 
 func PreviewUrl(id string) string {
@@ -12,4 +14,13 @@ func PreviewUrl(id string) string {
 // Note to self: file.WebContentLink = https://docs.google.com/uc?id=<id>&export=download
 func DownloadUrl(id string) string {
     return fmt.Sprintf("https://drive.google.com/uc?id=%s&export=download", id)
+}
+
+func ParentList(parents []*drive.ParentReference) string {
+    ids := make([]string, 0)
+    for _, parent := range parents {
+        ids = append(ids, parent.Id)
+    }
+
+    return strings.Join(ids, ", ")
 }
