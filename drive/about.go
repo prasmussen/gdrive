fc24aa48336d794998bf3e45356fb34721162b8a
Petter Rasmussen <petter.rasmussen@gmail.com>
2016-02-13 17:18:53 +0100

    Correct used space / free space

--- drive/about.go
+++ drive/about.go
@@ -21,8 +21,8 @@ func (self *Drive) About(args AboutArgs) (err error) {
     quota := about.StorageQuota
 
     fmt.Fprintf(args.Out, "User: %s, %s\n", user.DisplayName, user.EmailAddress)
-    fmt.Fprintf(args.Out, "Used: %s\n", formatSize(quota.UsageInDrive, args.SizeInBytes))
-    fmt.Fprintf(args.Out, "Free: %s\n", formatSize(quota.Limit - quota.UsageInDrive, args.SizeInBytes))
+    fmt.Fprintf(args.Out, "Used: %s\n", formatSize(quota.Usage, args.SizeInBytes))
+    fmt.Fprintf(args.Out, "Free: %s\n", formatSize(quota.Limit - quota.Usage, args.SizeInBytes))
     fmt.Fprintf(args.Out, "Total: %s\n", formatSize(quota.Limit, args.SizeInBytes))
     fmt.Fprintf(args.Out, "Max upload size: %s\n", formatSize(about.MaxUploadSize, args.SizeInBytes))
     return
