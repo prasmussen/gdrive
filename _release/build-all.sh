cabd73299fade0ff2fdc3732ebece151e9edf058
Petter Rasmussen <petter.rasmussen@gmail.com>
2016-02-22 20:46:53 +0100

    Add raspberry pi build

--- _release/build-all.sh
+++ _release/build-all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 APP_NAME="gdrive"
-PLATFORMS="darwin/386 darwin/amd64 darwin/arm darwin/arm64 dragonfly/amd64 freebsd/386 freebsd/amd64 freebsd/arm linux/386 linux/amd64 linux/arm linux/arm64 linux/ppc64 linux/ppc64le linux/mips64 linux/mips64le netbsd/386 netbsd/amd64 netbsd/arm openbsd/386 openbsd/amd64 openbsd/arm plan9/386 plan9/amd64 solaris/amd64 windows/386 windows/amd64"
+PLATFORMS="darwin/386 darwin/amd64 darwin/arm darwin/arm64 dragonfly/amd64 freebsd/386 freebsd/amd64 freebsd/arm linux/386 linux/amd64 linux/arm linux/arm64 linux/ppc64 linux/ppc64le linux/mips64 linux/mips64le linux/rpi netbsd/386 netbsd/amd64 netbsd/arm openbsd/386 openbsd/amd64 openbsd/arm plan9/386 plan9/amd64 solaris/amd64 windows/386 windows/amd64"
 
 BIN_PATH="_release/bin"
 
@@ -19,8 +19,16 @@ for PLATFORM in $PLATFORMS; do
         BIN_NAME="${BIN_NAME}.exe"
     fi
 
+    # Raspberrypi seems to need arm5 binaries
+    if [ $GOARCH == "rpi" ]; then
+        export GOARM=5
+        GOARCH="arm"
+    else
+        unset GOARM
+    fi
+
     export GOOS=$GOOS
-    export GOARCH=$GOARCH go build
+    export GOARCH=$GOARCH
 
     echo "Building $BIN_NAME"
     go build -ldflags '-w -s' -o ${BIN_PATH}/${BIN_NAME}
