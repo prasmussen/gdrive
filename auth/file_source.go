prasmussen/gdrive

commit b33b3e96eb6443ae3dff61e0e7b38bb529e328ea

New feature, support authentication via service account.

auth/file_source.go
@@ -31,7 +31,7 @@ func (self *fileSource) Token() (*oauth2.Token, error) {
 	return token, nil
 }
 
-func ReadToken(path string) (*oauth2.Token, bool, error) {
+func ReadFile(path string) ([]byte, bool, error) {
 	if !fileExists(path) {
 		return nil, false, nil
 	}
@@ -40,8 +40,19 @@ func ReadToken(path string) (*oauth2.Token, bool, error) {
 	if err != nil {
 		return nil, true, err
 	}
+	return content, true, nil
+}
+
+
+func ReadToken(path string) (*oauth2.Token, bool, error) {
+
+	content, exists, err := ReadFile(path)
+	if err != nil {
+		return nil, exists, err
+	}
+
 	token := &oauth2.Token{}
-	return token, true, json.Unmarshal(content, token)
+	return token, exists, json.Unmarshal(content, token)
 }
 
 func SaveToken(path string, token *oauth2.Token) error {

auth/oauth.go
@@ -3,6 +3,7 @@ package auth
 import (
 	"fmt"
 	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 	"net/http"
 	"time"
 )
@@ -62,6 +63,23 @@ func NewAccessTokenClient(clientId, clientSecret, accessToken string) *http.Clie
 		oauth2.NoContext,
 		conf.TokenSource(oauth2.NoContext, token),
 	)
+}
+
+func NewServiceAccountClient(serviceAccountFile string) (*http.Client, error) {
+	content, exists, err := ReadFile(serviceAccountFile)
+	if(!exists) {
+		return nil, fmt.Errorf("Service account filename %q not found", serviceAccountFile)
+	}
+
+	if(err != nil) {
+		return nil, err
+	}
+
+	conf, err := google.JWTConfigFromJSON(content, "https://www.googleapis.com/auth/drive")
+	if(err != nil) {
+		return nil, err
+	}
+	return conf.Client(oauth2.NoContext), nil
 }
 
 func getConfig(clientId, clientSecret string) *oauth2.Config {

gdrive.go
@@ -40,6 +40,11 @@ func main() {
 			Patterns:    []string{"--access-token"},
 			Description: "Oauth access token, only recommended for short-lived requests because of short lifetime (for advanced users)",
 		},
+		cli.StringFlag{
+			Name:        "serviceAccount",
+			Patterns:    []string{"--service-account"},
+			Description: "Oauth service account filename, used for server to server communication without user interaction (filename path is relative to config dir)",
+		},
 	}
 
 	handlers := []*cli.Handler{

handlers_drive.go
@@ -354,6 +354,16 @@ func getOauthClient(args cli.Arguments) (*http.Client, error) {
 	}
 
 	configDir := getConfigDir(args)
+
+	if args.String("serviceAccount") != "" {
+		serviceAccountPath := ConfigFilePath(configDir, args.String("serviceAccount"))
+		serviceAccountClient, err := auth.NewServiceAccountClient(serviceAccountPath)
+		if err != nil {
+			return nil, err
+		}
+		return serviceAccountClient, nil
+	}
+
 	tokenPath := ConfigFilePath(configDir, TokenFilename)
 	return auth.NewFileSourceClient(ClientId, ClientSecret, tokenPath, authCodePrompt)
 }

commit 2aa4234efa6e04fdeee3129acdaa3464aa170dcd

auth/file_source: don't try to read non-existent files
Commit b33b3e96e introduced a bug where we try to read a token file even if
it doesn't exist, causing unauthenticated run (e.g.  `gdrive about`) to fail
with the error

    Failed getting oauth client: Failed to read token: unexpected end of JSON input

Closes #257.

auth/file_source.go
@@ -47,7 +47,7 @@ func ReadFile(path string) ([]byte, bool, error) {
 func ReadToken(path string) (*oauth2.Token, bool, error) {
 
 	content, exists, err := ReadFile(path)
-	if err != nil {
+	if err != nil || exists == false {
 		return nil, exists, err
 	}
 