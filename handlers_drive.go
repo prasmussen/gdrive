6aa6705cd4f4ef1e722941803a612b4bb38bef8c
Exonip <41260107+Exonip@users.noreply.github.com>
2019-11-17 14:03:09 +0100

    Update handlers_drive.go
    Changed ClientId and ClientSecret

--- handlers_drive.go
+++ handlers_drive.go
@@ -14,8 +14,8 @@ import (
 	"github.com/prasmussen/gdrive/drive"
 )
 
-const ClientId = "367116221053-7n0vf5akeru7on6o2fjinrecpdoe99eg.apps.googleusercontent.com"
-const ClientSecret = "1qsNodXNaWq1mQuBjUjmvhoO"
+const ClientId = "847679045527-j6tit0q3okqlntuvqmsdamh0j0lcjckn.apps.googleusercontent.com"
+const ClientSecret = "DxgTv6LYEYgWQ_fewE4SPYmT"
 const TokenFilename = "token_v2.json"
 const DefaultCacheFileName = "file_cache.json"
 
